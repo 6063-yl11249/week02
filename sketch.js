11c65e3aaaf2d43e15a664d0d1b159869dc0cb73
Yuanqing <yl11249@nyu.edu>
2023-09-13 15:24:12 -0400

    added 2 ellipses and a rectangle

--- sketch.js
+++ sketch.js
@@ -1,5 +1,13 @@
 function setup() {
+createCanvas(600, 600);
+background(255, 0, 255);
 }
-
 function draw() {
+    fill("orange")
+    ellipse(500, 300, 100, 100)
+    fill('blue')
+    ellipse(200, 300, 300, 300)
+    stroke(255, 0, 0)
+    strokeWeight(4)
+    rect(300, 200, 200, 200)
 }
